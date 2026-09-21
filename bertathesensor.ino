5a29319a856fd76bd8ba6ac05c8b9390d76c872e
reksmeii <reksmei09@yahoo.com>
2026-09-21 12:59:28 -0700

    Add files via upload

---
+++ bertathesensor.ino
@@ -0,0 +1,84 @@
+/*
+  Ultrasonic Sensor HC-SR04 and Arduino Tutorial
+  by Dejan Nedelkovski,
+  www.HowToMechatronics.com
+*/
+
+// defines pins numbers
+const int trigPin = 11;
+const int echoPin = 12;
+
+// defines variables
+long duration;
+int distance;
+
+void setup() {
+  // UltraSonoc Pins
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(9600); // Starts the serial communication
+}
+
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+
+  while(distance > 50){
+    // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+
+    Serial.println("Waiting to detect an object.");
+    delay(250);
+  }
+  while(distance < 50){
+    // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+
+    Serial.println("Object is detected and present");
+    delay(250);
+    if(distance > 50){
+      Serial.println("Object left");
+    }
+  }
+  
+}
